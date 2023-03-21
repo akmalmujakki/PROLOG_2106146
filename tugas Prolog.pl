e0242070f709f2874fa64741ec4434916675b024
akmalmujakki <127036119+akmalmujakki@users.noreply.github.com>
2023-03-21 20:51:09 +0700

    Add files via upload

---
+++ tugas Prolog.pl
@@ -0,0 +1,10 @@
+menikah(David, Amy).
+menikah(Jack, Karen).
+anak(Amy, Liza).
+sodara(Liza, Jhon).
+anak(Karen, Ray).
+sodara(Ray, Susan).
+menikah(Jhon, Susan).
+anak(Jhon, Peter).
+sodara(Peter, Mary).
+
